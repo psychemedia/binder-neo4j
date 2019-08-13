5f1cf990554ae620f2f8f0b25ad84073c9b7860d
Tony Hirst <tony.hirst@gmail.com>
2019-08-13 13:23:36 +0100

    First proper commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # binder-neo4j
+
 Example of a Binderised repo running neo4j
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/psychemedia/binder-neo4j/master)
+
+Demo test code in the `py/` folder.
+
+(Since this repo also runs `jupytext`, if you  click on the `neo4j-demo.py` file from the notebook server `tree/` page and it will open *as a notebook*.)
+
+
+To run a container built from this repo locally:
+
+```
+pip install jupyter-repo2docker
+
+repo2docker https://github.com/psychemedia/binder-neo4j
+
+```
+
+Broken:
+
+- the neo4j GUI doesn't seem to work under local testing at least
---
+++ py/neo4j-demo.py
@@ -0,0 +1,21 @@
+# # neo4j - Demo
+#
+# Example of getting started with neo4j.
+#
+# Docs: https://py2neo.org/3.0/intro.html (although https://py2neo.org/2.0/intro.html#getting-connected is more useful, albeit with an older (broken) syntax in many places...).
+
+from py2neo import Graph
+graph = Graph(password="neo4jbinder")
+
+from py2neo import Node, Relationship
+
+alice = Node("Person", name="Alice")
+bob = Node("Person", name="Bob")
+alice_knows_bob = Relationship(alice, "KNOWS", bob)
+graph.create(alice_knows_bob)
+
+
+graph.run("CREATE (c:Person {name:{N}}) RETURN c", {"N": "Carol"})
+
+for record in graph.run("MATCH (p:Person) RETURN p.name AS name"):
+    print(record['name'])
