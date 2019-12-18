0fb77b05ca8b246cab6a34cbf34e86cee3d1b63d
Tony Hirst <tony.hirst@gmail.com>
2019-12-18 11:51:43 +0000

    Use external cypher magic repo

--- cypher_magic/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Cypher magic"""
-__version__ = '0.0.1'
-
-from .cypher import CypherMagic
-
-def load_ipython_extension(ipython):
-    ipython.register_magics(CypherMagic)
--- setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-
-setup(name='ipython_magic_cypher',
-      packages=['cypher_magic']
-)
